8f8e4b63a41660fd4cd3f49be2bf8551c8d33205
Luca Cantoreggi <luca@cn7r66.com>
2023-04-18 10:14:33 +0200

    Add ContainerCall provider.

---
+++ src/Container/Provider/ContainerCall.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace Vivarium\Container\Provider;
+
+use Vivarium\Container\Container;
+use Vivarium\Container\Key;
+use Vivarium\Container\Provider;
+
+final class ContainerCall implements Provider
+{
+    public function __construct(private Key $key) {}
+
+    public function provide(Container $container)
+    {
+        return $container->get($this->key);
+    }
+
+    public function getKey(): Key
+    {
+        return $this->key;
+    }
+}
