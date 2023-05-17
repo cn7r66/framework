a66978121173d6049d4dfcb1956fbaad2e5ee4f5
Luca Cantoreggi <luca@cn7r66.com>
2023-05-17 22:41:38 +0200

    Implement has and get

--- src/Container/Solver/Registry.php
+++ src/Container/Solver/Registry.php
@@ -1,17 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of Vivarium
  * SPDX-License-Identifier: MIT
  * Copyright (c) 2023 Luca Cantoreggi
- *
  */
 
 namespace Vivarium\Container\Solver;
 
+use OutOfBoundsException;
 use Vivarium\Collection\Map\HashMap;
 use Vivarium\Collection\Map\Map;
 use Vivarium\Container\Key;
+
+use function array_slice;
+use function count;
+use function explode;
+use function implode;
 
 /** @template T */
 final class Registry
@@ -28,7 +35,7 @@ final class Registry
     /** @param T $entry */
     public function add(Key $key, $entry): self
     {
-        $registry = clone $this;
+        $registry      = clone $this;
         $registry->map = $registry->map->put($key, $entry);
 
         return $registry;
@@ -37,13 +44,28 @@ final class Registry
     /** @return T */
     public function get(Key $key)
     {
+        if ($key->getTag() !== Key::DEFAULT) {
+            return $this->getExactly($key);
+        }
 
+        if ($key->getContext() !== Key::GLOBAL) {
+            do {
+                if ($this->hasExactly($key)) {
+                    return $this->getExactly($key);
+                }
+
+                $key = $this->widen($key);
+            } while ($key->getContext() !== Key::GLOBAL);
+        }
+
+        return $this->getExactly($key);
     }
 
+    /** @return T */
     public function getExactly(Key $key)
     {
-        if (! $this->has($key)) {
-            throw new \RuntimeException();
+        if (! $this->hasExactly($key)) {
+            throw new OutOfBoundsException('The provided key is not present.');
         }
 
         return $this->map->get($key);
@@ -51,11 +73,39 @@ final class Registry
 
     public function has(Key $key): bool
     {
-        return $this->map->containsKey($key);
+        if ($key->getTag() !== Key::DEFAULT) {
+            return $this->hasExactly($key);
+        }
+
+        if ($key->getContext() !== Key::GLOBAL) {
+            do {
+                if ($this->hasExactly($key)) {
+                    return true;
+                }
+
+                $key = $this->widen($key);
+            } while ($key->getContext() !== Key::GLOBAL);
+        }
+
+        return $this->hasExactly($key);
     }
 
     public function hasExactly(Key $key): bool
     {
+        return $this->map->containsKey($key);
+    }
 
+    private function widen(Key $key): Key
+    {
+        $context = explode('\\', $key->getContext());
+
+        if (count($context) === 1) {
+            return new Key($key->getType());
+        }
+
+        return new Key(
+            $key->getType(),
+            implode('\\', array_slice($context, 0, -1)),
+        );
     }
 }
