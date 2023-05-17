190ef55ed4773bf1aa112746c235ed7c8d61963a
Luca Cantoreggi <luca@cn7r66.com>
2023-05-17 12:49:31 +0200

    Add MultiStepSolverTest

---
+++ tests/Container/Solver/MultiStepSolverTest.php
@@ -0,0 +1,89 @@
+<?php declare(strict_types=1);
+
+namespace Vivarium\Test\Container\Solver;
+
+use PHPUnit\Framework\TestCase;
+use Vivarium\Container\Exception\KeyNotFound;
+use Vivarium\Container\Key;
+use Vivarium\Container\Provider;
+use Vivarium\Container\Solver\MultiStepSolver;
+use Vivarium\Container\Solver\SolverStep;
+
+/** @coversDefaultClass \Vivarium\Container\Solver\MultiStepSolver */
+final class MultiStepSolverTest extends TestCase
+{
+    /**
+     * @covers ::__container()
+     * @covers ::solve()
+     * @covers ::next()
+     */
+    public function testSolve(): void
+    {
+        $provider = static::createMock(Provider::class);
+
+        $step1 = static::createMock(SolverStep::class);
+        $step2 = static::createMock(SolverStep::class);
+
+        $step1->expects(static::once())
+              ->method('solve')
+              ->willReturnCallback(function (Key $key, callable $next): mixed {
+                  return $next($key);
+              });
+
+        $step2->expects(static::once())
+              ->method('solve')
+              ->willReturn($provider);
+
+        $solver = new MultiStepSolver(
+            $step1,
+            $step2,
+        );
+
+        static::assertSame($provider, $solver->solve(new Key('int')));
+    }
+
+    /**
+     * @covers ::__construct()
+     * @covers ::solve()
+     * @covers ::next()
+     */
+    public function testSolveEmptySolver(): void
+    {
+        static::expectException(KeyNotFound::class);
+
+        (new MultiStepSolver())
+            ->solve(new Key('int'));
+    }
+
+    /**
+     * @covers ::__construct()
+     * @covers ::solve()
+     * @covers ::next()
+     */
+    public function testSolveException(): void
+    {
+        static::expectException(KeyNotFound::class);
+
+        $step1 = static::createMock(SolverStep::class);
+        $step2 = static::createMock(SolverStep::class);
+
+        $step1->expects(static::once())
+            ->method('solve')
+            ->willReturnCallback(function (Key $key, callable $next): mixed {
+                return $next($key);
+            });
+
+        $step2->expects(static::once())
+            ->method('solve')
+            ->willReturnCallback(function (Key $key, callable $next): mixed {
+                return $next($key);
+            });
+
+        $solver = new MultiStepSolver(
+            $step1,
+            $step2,
+        );
+
+        $solver->solve(new Key('int'));
+    }
+}
