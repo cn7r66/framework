8587ed2084e7b7ac1a51735109bbcb6b3cf62da5
Luca Cantoreggi <luca@cn7r66.com>
2024-01-08 22:21:39 +0100

    Factory can be use as static method

--- src/Container/Provider/Factory.php
+++ src/Container/Provider/Factory.php
@@ -10,14 +10,17 @@ declare(strict_types=1);
 
 namespace Vivarium\Container\Provider;
 
+use Vivarium\Collection\Sequence\Sequence;
+use Vivarium\Container\Binder;
 use Vivarium\Container\Binding;
 use Vivarium\Container\Binding\ClassBinding;
 use Vivarium\Container\Container;
 use Vivarium\Container\Injection\MethodCall;
 use Vivarium\Container\Provider;
 use Vivarium\Container\Reflection\InstanceMethod;
+use Vivarium\Container\Reflection\StaticMethod;
 
-final class Factory implements Provider
+final class Factory implements StaticMethod, Provider
 {
     private Binding $factory;
 
@@ -63,5 +66,45 @@ final class Factory implements Provider
             $this->method : $this->method
                 ->bindParameter($this->requester)
                 ->toInstance($requester);
+    }
+
+    public function getClass(): string
+    {
+        // TODO: Implement getClass() method.
+    }
+
+    public function getName(): string
+    {
+        // TODO: Implement getName() method.
+    }
+
+    public function bindParameter(string $parameter): Binder
+    {
+        // TODO: Implement bindParameter() method.
+    }
+
+    public function getParameter(string $parameter): Provider
+    {
+        // TODO: Implement getParameter() method.
+    }
+
+    public function hasParameter(string $parameter): bool
+    {
+        // TODO: Implement hasParameter() method.
+    }
+
+    public function getArguments(): Sequence
+    {
+        // TODO: Implement getArguments() method.
+    }
+
+    public function getArgumentsValue(Container $container): Sequence
+    {
+        // TODO: Implement getArgumentsValue() method.
+    }
+
+    public function invoke(Container $container): mixed
+    {
+        // TODO: Implement invoke() method.
     }
 }
