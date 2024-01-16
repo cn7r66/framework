fc82c95c2f12dd18464a585eb42c9894ca710a24
Luca Cantoreggi <luca@cn7r66.com>
2024-01-16 01:18:06 +0100

    Add ReflectionSolver

---
+++ src/Container/ReflectionSolver.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Vivarium
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2023 Luca Cantoreggi
+ */
+
+namespace Vivarium\Container\Step;
+
+use Vivarium\Container\Binding;
+use Vivarium\Container\Provider;
+use Vivarium\Container\Step;
+use Vivarium\Container\Binding\ClassBinding;
+use ReflectionClass;
+use Vivarium\Container\Provider\Prototype;
+use ReflectionException;
+use RuntimeException;
+
+final class ReflectionSolver implements Step
+{
+    public function solve(Binding $request, callable $next): Provider
+    {
+        try {
+            $binding   = ClassBinding::fromBinding($request);
+            $reflector = new ReflectionClass($binding->getId());
+            if (! $reflector->isInstantiable()) {
+                return $next();
+            }
+
+            return new Prototype($binding->getId());
+        } catch (ReflectionException) {
+            throw new RuntimeException();
+        }
+    }
+}
