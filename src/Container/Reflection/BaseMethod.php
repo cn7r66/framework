64bcf9465fdf4c86f072179feaf0f99b69243d2e
Luca Cantoreggi <luca@cn7r66.com>
2024-01-19 12:55:17 +0100

    Fix HasMethod assertion

--- src/Container/Reflection/BaseMethod.php
+++ src/Container/Reflection/BaseMethod.php
@@ -1,9 +1,4 @@
 <?php
-/*
- * This file is part of Vivarium
- * SPDX-License-Identifier: MIT
- * Copyright (c) 2023 Luca Cantoreggi
- */
 
 declare(strict_types=1);
 
@@ -40,8 +35,8 @@ abstract class BaseMethod implements Method
         (new IsClassOrInterface())
             ->assert($class);
 
-        (new HasMethod($class))
-            ->assert($method);
+        (new HasMethod($method))
+            ->assert($class);
 
         $this->parameters = new HashMap();
     }
