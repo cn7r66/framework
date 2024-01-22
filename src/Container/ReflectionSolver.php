c09473e6d2d68d43a804f6fdab59391989bfda5b
Luca Cantoreggi <luca@cn7r66.com>
2024-01-22 10:38:13 +0100

    Remove try catch block

--- src/Container/ReflectionSolver.php
+++ src/Container/ReflectionSolver.php
@@ -11,28 +11,23 @@ declare(strict_types=1);
 namespace Vivarium\Container;
 
 use ReflectionClass;
-use ReflectionException;
-use RuntimeException;
-use Vivarium\Container\Binding;
 use Vivarium\Container\Binding\ClassBinding;
-use Vivarium\Container\Provider;
 use Vivarium\Container\Provider\Prototype;
-use Vivarium\Container\Step;
 
 final class ReflectionSolver implements Step
 {
     public function solve(Binding $request, callable $next): Provider
     {
-        try {
-            $binding   = ClassBinding::fromBinding($request);
-            $reflector = new ReflectionClass($binding->getId());
-            if (! $reflector->isInstantiable()) {
-                return $next();
-            }
-
-            return new Prototype($binding->getId());
-        } catch (ReflectionException) {
-            throw new RuntimeException();
+        if (! class_exists($request->getId())) {
+            return $next();
         }
+
+        $binding = ClassBinding::fromBinding($request);
+        $reflector = new ReflectionClass($binding->getId());
+        if (! $reflector->isInstantiable()) {
+            return $next();
+        }
+
+        return new Prototype($binding->getId());
     }
 }
