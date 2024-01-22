da247a28b193b9da2152bced332fe75ccf86d549
Luca Cantoreggi <luca@cn7r66.com>
2024-01-22 11:48:44 +0100

    Add SubContainer

---
+++ src/Container/SubContainer.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Vivarium
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2023 Luca Cantoreggi
+ */
+
+namespace Vivarium\Container;
+
+use Psr\Container\ContainerInterface;
+use Vivarium\Container\Provider\Instance;
+
+final class SubContainer implements Step
+{
+    public function __construct(private ContainerInterface $container)
+    {}
+
+    /** @param callable(): Provider $next */
+    public function solve(Binding $request, callable $next): Provider
+    {
+        if (! $this->container->has($request->getId())) {
+            return $next();
+        }
+
+        return new Instance(
+            $this->container->get($request->getId())
+        );
+    }
+}
