64007fe38f2accbabcc8227266f55d1479f139c4
Luca Cantoreggi <luca@cn7r66.com>
2024-01-23 12:46:27 +0100

    Add more tests

--- tests/Container/Reflection/ConstructorTest.php
+++ tests/Container/Reflection/ConstructorTest.php
@@ -15,6 +15,10 @@ use Vivarium\Test\Container\Stub\ConcreteStub;
 use Vivarium\Container\Reflection\Constructor;
 use Vivarium\Container\Container;
 use PHPUnit\Framework\MockObject\MockObject;
+use Vivarium\Container\Binding\TypeBinding;
+use Vivarium\Container\Exception\ParameterNotFound;
+use Vivarium\Test\Container\Stub\SimpleStub;
+use Vivarium\Container\Provider;
 
 /** @coversDefaultClass Vivarium\Container\Reflection\Constructor */
 final class ConstructorTest extends TestCase
@@ -35,5 +39,66 @@ final class ConstructorTest extends TestCase
                   ->method('get');
 
         static::assertInstanceOf(ConcreteStub::class, $constructor->invoke($container));
+    }
+
+    /** @covers ::getName() */
+    public function testGetName(): void
+    {
+        $constructor = new Constructor(ConcreteStub::class);
+
+        static::assertSame('__construct', $constructor->getName());
+    }
+
+    /**
+     * @covers ::bindParameter()
+     * @covers ::hasParameter()
+     * @covers ::getParameter()
+     */
+    public function testBindParameter(): void
+    {
+        $constructor = (new Constructor(SimpleStub::class))
+                                    ->bindParameter('stub')
+                                    ->to(ConcreteStub::class);
+
+        static::assertTrue($constructor->hasParameter('stub'));
+        static::assertInstanceOf(Provider::class, $constructor->getParameter('stub'));
+    }
+
+    /**
+     * @covers Vivarium\Container\Reflection\BaseMethod::__construct()
+     * @covers ::__construct()
+     * @covers ::bindParameter()
+     * @covers ::getArguments()
+     * @covers ::getArgumentsValue()
+     * @covers ::invoke()
+     */
+    public function testInvokeWithParameterResolution(): void
+    {
+        $constructor = (new Constructor(SimpleStub::class))
+        ->bindParameter('stub')
+        ->to(ConcreteStub::class);
+
+        /** @var MockObject&Container */
+        $container = $this->getMockBuilder(Container::class)
+        ->getMock();
+
+        $container->expects(static::once())
+        ->method('get')
+        ->with(static::equalTo(new TypeBinding(ConcreteStub::class)))
+        ->willReturn(new ConcreteStub());
+
+        static::assertInstanceOf(SimpleStub::class, $constructor->invoke($container));
+    }
+
+    /**
+     * @covers ::getParameter()
+     */
+    public function testGetParameterException(): void
+    {
+        static::expectException(ParameterNotFound::class);
+        static::expectExceptionMessage('Parameter named stub in method __construct not found.');
+
+        $constructor = new Constructor(SimpleStub::class);
+        $constructor->getParameter('stub');
     }
 }
