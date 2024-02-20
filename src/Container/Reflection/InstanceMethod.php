777bebb39a4a9f60cb9cb56d6949ec628fc3df76
Luca Cantoreggi <luca@cn7r66.com>
2024-02-20 11:23:54 +0100

    Remove Injection interface

--- src/Container/Reflection/InstanceMethod.php
+++ src/Container/Reflection/InstanceMethod.php
@@ -13,7 +13,7 @@ namespace Vivarium\Container\Reflection;
 use Vivarium\Container\Container;
 use Vivarium\Container\Injection;
 
-interface InstanceMethod extends Method, Injection
+interface InstanceMethod extends Method
 {
     public function invoke(Container $container, object $instance): mixed;
 }
