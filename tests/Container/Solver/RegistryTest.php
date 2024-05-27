1a943f2cd673020c37245402b011f6510817464d
Luca Cantoreggi <luca@cn7r66.com>
2024-05-27 11:35:58 +0200

    Add RegistryTest boilerplate

---
+++ tests/Container/Solver/RegistryTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of Vivarium
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2021 Luca Cantoreggi
+ */
+
+declare(strict_types=1);
+
+namespace Vivarium\Test\Container\Solver;
+
+use PHPUnit\Framework\TestCase;
+use Vivarium\Container\Solver\Registry;
+use Vivarium\Container\Reflection\InstanceMethod;
+
+/** @coversDefaultClass \Vivarium\Container\RegistryTest */
+final class RegistryTest extends TestCase
+{
+    public function testIntercept(): void
+    {
+    }
+}
